42e44af56fad15e85852d00979455c8d149a0f6c
Magesh Nandikkara <X00218707@mytudublin.ie>
2025-04-11 22:53:51 +0100

    Fixing Build Issues

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa514bc305fde38a89e4f7b0183948390d270275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df06d381525d722a2c815663de95b47f9dbe7907")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
